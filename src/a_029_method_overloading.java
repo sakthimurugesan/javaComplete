c481b1520a44912a37e0f7ed423115e3bc0e83b9
SAKTHI M <msakthi150@gmail.com>
2023-10-25 09:48:27 +0530

    files added

--- src/a_029_method_overloading.java
+++ src/a_029_method_overloading.java
@@ -2,8 +2,48 @@
  * a_029_method_overloading
  */
 public class a_029_method_overloading {
+    public static int maximum(int a,int b)
+    {
+        System.out.println("int maximum method called");
+        return a>b?a:b;
+    }
+
+    public static float maximum(float a,float b)
+    {
+        System.out.println("float maximum method called");
+        return a>b?a:b;
+    }
+
+    public static double maximum(double a,double b)
+    {
+        System.out.println("double maximum method called");
+        return a>b?a:b;
+    }
 
     public static void main(String[] args) {
-        
+        /*
+         method overloading in java is concept that more methods have same name
+         but different return type and different length of arguements and different datatype of arguements
+
+         rules
+         1.same method name
+         2.different return type
+         3.no of arguements length should vary
+         4.data type of arguements should vary
+
+         method overloading is used when we need to do same operations with different type of datas
+         like finding maximum of two number
+         we will find max of two number with data types like float,long,double,int,short,byte
+         */
+
+         System.out.println(maximum(5.36D, 98.65D));
+         System.out.println(maximum(5.36, 98.65)); // this will also call double maximum method because default number without decimal values are double in java for float we need to mention f
+         System.out.println(maximum(5.36f, 98.65f));
+         System.out.println(maximum(5, 98));
+         /*
+          int maximum(int a,int b,int c)
+          int maximum(int a,int b,int c,int d)
+          int maximum(int a,int b,int c,int d,int e)
+          */
     }
 }
--- src/a_030_variable_arguements.java
+++ src/a_030_variable_arguements.java
@@ -1,9 +1,49 @@
-/**
- * a_030_variable_arguements
- */
+/*
+a_030_variable_arguements
+*/
 public class a_030_variable_arguements {
 
+    public static int max(int a,int []b)
+    {
+        int m=Integer.MIN_VALUE;
+        if(a>m)
+        m=a;
+        for(int c:b)
+        if(c>m)
+        m=c;
+        return m;
+    }
     public static void main(String[] args) {
-        
+        /*
+        we had previously seen method overloading with different no of parameters
+        for example we use maximum method to max of two numbers sometimes it may three,four etc
+        it such case we cannot define same method for multiple times
+        instead we can use variable arguements
+
+        variable arguments is just like passing an array of elements of same type but there is an differnet while calling the methods
+
+        syntax
+
+        int max(int a,int ...b)
+        here ...b is variable length arguement
+        max(2,3,4,6,7,4,56,5,44,6,5,4)
+        here 2 is assigned to a
+        and remaining are assigned to b array
+
+        here we can also call this method as max(2,array_variable)
+        also like max(2,new int[]{3,4,6,7,4,56,5,44,6,5,4})
+
+        we can also call max(2) it will not show any errors
+
+        we can also declare using array int max(int a,int b[])
+        call this method as max(2,new int[]{3,4,6,7,4,56,5,44,6,5,4})
+        max(2,array_variable)
+         we can call only this not as
+        max(2,3,4,6,7,4,56,5,44,6,5,4) this is invalid
+
+        max(2) this will show error
+         */
+        int arr[]={3,4,6,7,4,56,5,44,6,5,4};
+        System.out.println(max(2,new int[]{3,4,6,7,4,56,5,44,6,5,4}));
     }
 }
